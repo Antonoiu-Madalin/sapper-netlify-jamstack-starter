1e5bd6e329bd1f4295fb7e0ee587d99427ddcd3b
@mantonoiu <antonoiumadalin@gmail.com>
2019-08-07 05:57:22 +0300

    Super slick! Want this
    Added colors for tabs and much more!

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -4,150 +4,285 @@
 
 
 <style>
-		@import url('https://fonts.googleapis.com/css?family=Open+Sans:400,600,700');
-		@import url('https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+	@import url("https://fonts.googleapis.com/css?family=Open+Sans:400,600,700");
+	@import url("https://use.fontawesome.com/releases/v5.1.0/css/all.css");
 
-		*,*::before,*::after{margin:0;padding:0;box-sizing:border-box;}
-		html,body{height:100vh;}
-		body{
-		display:flex;
-		align-items:center;
-		justify-content:center;
-		padding:40px;
-		font:14px/1.5 'Open Sans', sans-serif;
-		}
+	*,
+	*::before,
+	*::after {
+	  margin: 0;
+	  padding: 0;
+	  box-sizing: border-box;
+	}
 
-		p:not(:last-child) {
-			margin: 0 0 20px;
-		}
+	.grid {
+	  height: auto; /*or 100vh if you want full screen */
+	  max-width: 900px;
+	  display: grid;
+	  grid-template-columns: 100px auto;
+	  grid-gap: 10px;
+	  align-items: top;
+	  justify-content: top;
+	  padding: 20px;
+	  font: 14px/1.5 "Open Sans", sans-serif;
+	}
 
-		main {
-			max-width: 800px;
-			padding: 40px;
-			border: 1px solid rgba(0,0,0,.2);
-			color: #fff;
-			box-shadow: 0 1px 3px rgba(0,0,0,.15);
-		}
+	p:not(:last-child) {
+	  margin: 0 0 20px;
+	}
 
-		section {
-			display: none;
-			padding: 20px 0 0;
-			 border-top: 1px solid #18222E;
-		}
+	.box-a,
+	.box-b {
+	  padding: 40px;
+	  border: 1px solid rgba(0, 0, 0, 0.2);
+	  color: #fff;
+	  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.15);
+	}
+	.box-a-rotate {
+	  -webkit-transform: rotate(90deg);
+	  -moz-transform: rotate(90deg);
+	  -o-transform: rotate(90deg);
+	  -ms-transform: rotate(90deg);
+	  transform: rotate(90deg);
+	}
 
-		input {
-			display: none;
-		}
+	section {
+	  display: none;
+	  padding: 20px 0 0;
+	  border-top: 1px solid #18222e;
+	}
 
-		label {
-			display: inline-block;
-			margin: 0 0 -1px;
-			padding: 15px 25px;
-			font-weight: 600;
-			text-align: center;
-			color: #abc;
-			border: 1px solid transparent;
-			
-		}
+	input {
+	  display: none;
+	}
 
-		label:before {
-			font-family: fontawesome;
-			font-weight: normal;
-			margin-right: 10px;
-		}
+	label {
+	  display: inline-block;
+	  margin: 0 0 -1px;
+	  padding: 15px 25px;
+	  font-weight: 600;
+	  text-align: center;
+	  color: #abc;
+	  border: 1px solid transparent;
+	}
 
-		label[for*='1']:before { content: '\f41b'; }
-		label[for*='2']:before { content: '\f17d'; }
-		label[for*='3']:before { content: '\f16c'; }
-		label[for*='4']:before { content: '\f171'; }
+	label:before {
+	  font-family: fontawesome;
+	  font-weight: normal;
+	  margin-right: 10px;
+	}
 
-		label:hover {
-			color: #789;
-			cursor: pointer;
-		}
+	label[for*="0"]:before {
+	  font-family: "Font Awesome 5 Brands";
+	  content: "\f3b9";
+	  font-size: 40px;
+	  color: #f0db4f;
+	}
 
-		input:checked + label {
-			color: #0af;
-			border: 1px solid #18222E;
-			border-top: 2px solid #0af;
-			border-bottom: 1px solid #10161E;
-		}
+	label[for*="1"]:before {
+	  font-family: "Font Awesome 5 Brands";
+	  content: "\f41b";
+	}
+	label[for*="2"]:before {
+	  font-family: "Font Awesome 5 Brands";
+	  content: "\f41f";
+	}
 
-		#tab1:checked ~ #content1,
-		#tab2:checked ~ #content2,
-		#tab3:checked ~ #content3,
-		#tab4:checked ~ #content4 {
-			display: block;
-		}
+	label[for*="3"]:before {
+	  font-family: "Font Awesome 5 Brands";
+	  content: "\f420";
+	}
 
-		@media screen and (max-width: 800px) {
-			label {
-				font-size: 0;
-			}
-			label:before {
-				margin: 0;
-				font-size: 18px;
-			}
-		}
+	label[for*="4"]:before {
+	  font-family: "Font Awesome 5 Brands";
+	  content: "\f423";
+	}
 
-		@media screen and (max-width: 500px) {
-			label {
-				padding: 15px;
-			}
-		}
+	label:hover {
+	  color: #789;
+	  cursor: pointer;
+	}
+
+	input:checked + label[for*="1"] {
+	  color: #0af;
+	  border: 1px solid #18222e;
+	  border-top: 2px solid #0af;
+	  border-bottom: 1px solid #10161e;
+	}
+
+	input:checked + label[for*="2"] {
+	  color: #42b883;
+	  border: 1px solid #18222e;
+	  border-top: 2px solid #42b883;
+	  border-bottom: 1px solid #10161e;
+	}
+
+	input:checked + label[for*="3"] {
+	  color: #b52e31;
+	  border: 1px solid #18222e;
+	  border-top: 2px solid #b52e31;
+	  border-bottom: 1px solid #10161e;
+	}
+
+	input:checked + label[for*="4"] {
+	  color: #f23819;
+	  border: 1px solid #18222e;
+	  border-top: 2px solid #f23819;
+	  border-bottom: 1px solid #10161e;
+	}
+
+	#tab1:checked ~ #content1,
+	#tab2:checked ~ #content2,
+	#tab3:checked ~ #content3,
+	#tab4:checked ~ #content4 {
+	  display: block;
+	}
+
+	@media screen and (max-width: 800px) {
+	  label {
+	    font-size: 0;
+	  }
+	  label:before {
+	    margin: 0;
+	    font-size: 18px;
+	  }
+	}
+
+	@media screen and (max-width: 500px) {
+	  label {
+	    padding: 15px;
+	  }
+	}
 </style>
 
 
+<main class="grid">
+		<div class="box-a" style="font-size: 30px;">
+			<div class="box-a-rotate">
+				<input id="tab0" type="radio" name="main-tabs" checked>
+				<label for="tab0"><span style="color: #F0DB4F">JavaScript</span></label>
+			</div>
+		</div>
 
+		<div class="box-b">
+			<input id="tab1" type="radio" name="tabs" checked>
+			<label for="tab1">React.JS</label>
 
-<main>
+			<input id="tab2" type="radio" name="tabs">
+			<label for="tab2">Vue.JS</label>
 
-  <input id="tab1" type="radio" name="tabs" checked>
-  <label for="tab1">Codepen</label>
+			<input id="tab3" type="radio" name="tabs">
+			<label for="tab3" >Angular.JS</label>
 
-  <input id="tab2" type="radio" name="tabs">
-  <label for="tab2">Dribbble</label>
+			<input id="tab4" type="radio" name="tabs">
+			<label for="tab4">Ember.JS</label>
 
-  <input id="tab3" type="radio" name="tabs">
-  <label for="tab3">Stack Overflow</label>
+			<section id="content1">
 
-  <input id="tab4" type="radio" name="tabs">
-  <label for="tab4">Bitbucket</label>
+					<ul style="padding-left: 17px">
+						<li>Gatsby.js</li>
+						<li>Next.js</li>
+					</ul>
+					<br>
+					<p>
+						Jerky jowl pork chop tongue, kielbasa shank venison. Capicola shank pig ribeye leberkas filet mignon brisket beef kevin tenderloin porchetta. Capicola fatback venison shank kielbasa, drumstick ribeye landjaeger beef kevin tail meatball pastrami prosciutto pancetta. Tail kevin spare ribs ground round ham ham hock brisket shoulder. Corned beef tri-tip leberkas flank sausage ham hock filet mignon beef ribs pancetta turkey.
+					</p>
+					<p>
+						Bacon ipsum dolor sit amet landjaeger sausage brisket, jerky drumstick fatback boudin.
+					</p>
+			</section>
 
-  <section id="content1">
-    <p>
-      Jerky jowl pork chop tongue, kielbasa shank venison. Capicola shank pig ribeye leberkas filet mignon brisket beef kevin tenderloin porchetta. Capicola fatback venison shank kielbasa, drumstick ribeye landjaeger beef kevin tail meatball pastrami prosciutto pancetta. Tail kevin spare ribs ground round ham ham hock brisket shoulder. Corned beef tri-tip leberkas flank sausage ham hock filet mignon beef ribs pancetta turkey.
-    </p>
-    <p>
-      Bacon ipsum dolor sit amet landjaeger sausage brisket, jerky drumstick fatback boudin.
-    </p>
-  </section>
+			<section id="content2">
+				<p>
+					Bacon ipsum dolor sit amet landjaeger sausage brisket, jerky drumstick fatback boudin.
+				</p>
+				<p>
+					Jerky jowl pork chop tongue, kielbasa shank venison. Capicola shank pig ribeye leberkas filet mignon brisket beef kevin tenderloin porchetta. Capicola fatback venison shank kielbasa, drumstick ribeye landjaeger beef kevin tail meatball pastrami prosciutto pancetta. Tail kevin spare ribs ground round ham ham hock brisket shoulder. Corned beef tri-tip leberkas flank sausage ham hock filet mignon beef ribs pancetta turkey.
+				</p>
+			</section>
 
-  <section id="content2">
-    <p>
-      Bacon ipsum dolor sit amet landjaeger sausage brisket, jerky drumstick fatback boudin.
-    </p>
-    <p>
-      Jerky jowl pork chop tongue, kielbasa shank venison. Capicola shank pig ribeye leberkas filet mignon brisket beef kevin tenderloin porchetta. Capicola fatback venison shank kielbasa, drumstick ribeye landjaeger beef kevin tail meatball pastrami prosciutto pancetta. Tail kevin spare ribs ground round ham ham hock brisket shoulder. Corned beef tri-tip leberkas flank sausage ham hock filet mignon beef ribs pancetta turkey.
-    </p>
-  </section>
+			<section id="content3">
+				<p>
+					Jerky jowl pork chop tongue, kielbasa shank venison. Capicola shank pig ribeye leberkas filet mignon brisket beef kevin tenderloin porchetta. Capicola fatback venison shank kielbasa, drumstick ribeye landjaeger beef kevin tail meatball pastrami prosciutto pancetta. Tail kevin spare ribs ground round ham ham hock brisket shoulder. Corned beef tri-tip leberkas flank sausage ham hock filet mignon beef ribs pancetta turkey.
+				</p>
+				<p>
+					Bacon ipsum dolor sit amet landjaeger sausage brisket, jerky drumstick fatback boudin.
+				</p>
+			</section>
 
-  <section id="content3">
-    <p>
-      Jerky jowl pork chop tongue, kielbasa shank venison. Capicola shank pig ribeye leberkas filet mignon brisket beef kevin tenderloin porchetta. Capicola fatback venison shank kielbasa, drumstick ribeye landjaeger beef kevin tail meatball pastrami prosciutto pancetta. Tail kevin spare ribs ground round ham ham hock brisket shoulder. Corned beef tri-tip leberkas flank sausage ham hock filet mignon beef ribs pancetta turkey.
-    </p>
-    <p>
-      Bacon ipsum dolor sit amet landjaeger sausage brisket, jerky drumstick fatback boudin.
-    </p>
-  </section>
-
-  <section id="content4">
-    <p>
-      Bacon ipsum dolor sit amet landjaeger sausage brisket, jerky drumstick fatback boudin.
-    </p>
-    <p>
-      Jerky jowl pork chop tongue, kielbasa shank venison. Capicola shank pig ribeye leberkas filet mignon brisket beef kevin tenderloin porchetta. Capicola fatback venison shank kielbasa, drumstick ribeye landjaeger beef kevin tail meatball pastrami prosciutto pancetta. Tail kevin spare ribs ground round ham ham hock brisket shoulder. Corned beef tri-tip leberkas flank sausage ham hock filet mignon beef ribs pancetta turkey.
-    </p>
-  </section>
-
+			<section id="content4">
+				<p>
+					Bacon ipsum dolor sit amet landjaeger sausage brisket, jerky drumstick fatback boudin.
+				</p>
+				<p>
+					Jerky jowl pork chop tongue, kielbasa shank venison. Capicola shank pig ribeye leberkas filet mignon brisket beef kevin tenderloin porchetta. Capicola fatback venison shank kielbasa, drumstick ribeye landjaeger beef kevin tail meatball pastrami prosciutto pancetta. Tail kevin spare ribs ground round ham ham hock brisket shoulder. Corned beef tri-tip leberkas flank sausage ham hock filet mignon beef ribs pancetta turkey.
+				</p>
+			</section>
+	</div>
 </main>
+
+<!-- Make radio and chose based on JavaScript, Php etc to show, with {#if}-->
+<!--<main class="grid">
+		<div class="box-a" style="font-size: 30px;">
+			<div class="box-a-rotate">
+				<input id="tab10" type="radio" name="main-tabs-secondary">
+				<label for="tab10"><span style="color: #4f5b93;">PHP</span></label>
+			</div>
+		</div>
+
+		<div class="box-b">
+			<input id="tab11" type="radio" name="tabs-secondary">
+			<label for="tab11">Laravel</label>
+
+			<input id="tab12" type="radio" name="tabs-secondary">
+			<label for="tab12">Vue</label>
+
+			<input id="tab13" type="radio" name="tabs-secondary">
+			<label for="tab13" >Angular</label>
+
+			<input id="tab14" type="radio" name="tabs-secondary">
+			<label for="tab14">PHP</label>
+
+			<section id="content11">
+
+					<ul style="padding-left: 17px">
+						<li>Gatsby.js</li>
+						<li>Next.js</li>
+					</ul>
+					<br>
+					<p>
+						Jerky jowl pork chop tongue, kielbasa shank venison. Capicola shank pig ribeye leberkas filet mignon brisket beef kevin tenderloin porchetta. Capicola fatback venison shank kielbasa, drumstick ribeye landjaeger beef kevin tail meatball pastrami prosciutto pancetta. Tail kevin spare ribs ground round ham ham hock brisket shoulder. Corned beef tri-tip leberkas flank sausage ham hock filet mignon beef ribs pancetta turkey.
+					</p>
+					<p>
+						Bacon ipsum dolor sit amet landjaeger sausage brisket, jerky drumstick fatback boudin.
+					</p>
+			</section>
+
+			<section id="content12">
+				<p>
+					Bacon ipsum dolor sit amet landjaeger sausage brisket, jerky drumstick fatback boudin.
+				</p>
+				<p>
+					Jerky jowl pork chop tongue, kielbasa shank venison. Capicola shank pig ribeye leberkas filet mignon brisket beef kevin tenderloin porchetta. Capicola fatback venison shank kielbasa, drumstick ribeye landjaeger beef kevin tail meatball pastrami prosciutto pancetta. Tail kevin spare ribs ground round ham ham hock brisket shoulder. Corned beef tri-tip leberkas flank sausage ham hock filet mignon beef ribs pancetta turkey.
+				</p>
+			</section>
+
+			<section id="content13">
+				<p>
+					Jerky jowl pork chop tongue, kielbasa shank venison. Capicola shank pig ribeye leberkas filet mignon brisket beef kevin tenderloin porchetta. Capicola fatback venison shank kielbasa, drumstick ribeye landjaeger beef kevin tail meatball pastrami prosciutto pancetta. Tail kevin spare ribs ground round ham ham hock brisket shoulder. Corned beef tri-tip leberkas flank sausage ham hock filet mignon beef ribs pancetta turkey.
+				</p>
+				<p>
+					Bacon ipsum dolor sit amet landjaeger sausage brisket, jerky drumstick fatback boudin.
+				</p>
+			</section>
+
+			<section id="content14">
+				<p>
+					Bacon ipsum dolor sit amet landjaeger sausage brisket, jerky drumstick fatback boudin.
+				</p>
+				<p>
+					Jerky jowl pork chop tongue, kielbasa shank venison. Capicola shank pig ribeye leberkas filet mignon brisket beef kevin tenderloin porchetta. Capicola fatback venison shank kielbasa, drumstick ribeye landjaeger beef kevin tail meatball pastrami prosciutto pancetta. Tail kevin spare ribs ground round ham ham hock brisket shoulder. Corned beef tri-tip leberkas flank sausage ham hock filet mignon beef ribs pancetta turkey.
+				</p>
+			</section>
+	</div>
+</main>-->
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,142 +8,158 @@
       .then(response => response.json())
       .then(json => console.log(json));
   });
- 
-
 </script>
  
 <style>
-  section{}
-    /* Portfolio important settings */
-    .portfolio-block{
-        padding-top:100px;
-        text-align:center;
-        padding-left: 15px;
-        padding-right: 15px;
-    }
-    /* Must be placed right after. Clears text on lower screens */
-    @media (max-width: 576px) {
-        .portfolio-block {
-            padding-top:50px; 
-        }
-      .portfolio-block.block-intro .about-me{
-          width: 320px; 
-        }
-    }
-    .portfolio-block.block-intro .about-me{
-        max-width:750px;
-        margin:0 auto;
-        padding-top: 8px;
-        padding-bottom: 0px;
-    }
-    .portfolio-block.block-intro p{
-        font-size:1.5em;
-        font-weight:300;
-        margin-top: 20px;
-    }
-    .portfolio-block.block-intro .avatar{
-        width:150px;
-        height:150px;
-        background-size:cover;
-        background-repeat:no-repeat;
-        margin:auto;
-        border-radius:100px;
-        margin-bottom:30px;
-    }
-    
-    /* Button theme- rename */
-      .theme-6 {
-      --color-button-bg: #f7d337;
-      --radius-button: 40px;
-        padding-top: 20px;
-        outline: none;
-            
-        }
- 
-
-    /*Experimental avatar*/  
-    .avatar {
-      height: 200px;
-      position: relative;
-      width: 200px;
-    }
-    .avatar img {
-      border-radius: 9999px;
-      height: 100%;
-      position: relative;
-      width: 100%;
-      z-index: 2;
-    }
-    
-    .grid__item{
-        outline: none;
-    }
-    
-    
-    @keyframes cycle-colors {
-      0% { border-color: hsl(0, 100%, 50%); }
-      25% { border-color: hsl(90, 100%, 50%); }
-      50% { border-color: hsl(180, 100%, 50%); }
-      75% { border-color: hsl(270, 100%, 50%); }
-      100% { border-color: hsl(360, 100%, 50%); }
-    }
-    @keyframes pulse {
-      to {
-        opacity: 0;
-        transform: scale(1);
-      }
-    }
-
-    .avatar::before,
-    .avatar::after {
-      animation: pulse 5s linear infinite;
-      border: #fff solid 5px;
-      border-radius: 99999px;
-      box-sizing: border-box;
-      content: ' ';
-      height: 140%;
-      left: -20%;
-      opacity: .6;
-      position: absolute;
-      top: -20%;
-      transform: scale(0.714);
-      width: 140%;
-      z-index: 1;
-    }
-    .avatar::after {
-      animation-delay: 2s;
-    }
-    
-    /* Fixed anchor click only button and centered*/
-    .anchorControl{
-        width: 107px;
-        text-align: center;
-        display: table;
-        margin: 0 auto;
+  section {
   }
-    
+  /* Portfolio important settings */
+  .portfolio-block {
+    padding-top: 100px;
+    text-align: center;
+    padding-left: 15px;
+    padding-right: 15px;
+  }
+  /* Must be placed right after. Clears text on lower screens */
+  @media (max-width: 576px) {
+    .portfolio-block {
+      padding-top: 50px;
+    }
+    .portfolio-block.block-intro .about-me {
+      width: 320px;
+    }
+  }
+  .portfolio-block.block-intro .about-me {
+    max-width: 750px;
+    margin: 0 auto;
+    padding-top: 8px;
+    padding-bottom: 0px;
+  }
+  .portfolio-block.block-intro p {
+    font-size: 1.5em;
+    font-weight: 300;
+    margin-top: 20px;
+  }
+  .portfolio-block.block-intro .avatar {
+    width: 150px;
+    height: 150px;
+    background-size: cover;
+    background-repeat: no-repeat;
+    margin: auto;
+    border-radius: 100px;
+    margin-bottom: 30px;
+  }
+
+  /* Button theme- rename */
+  .theme-6 {
+    --color-button-bg: #f7d337;
+    --radius-button: 40px;
+    padding-top: 20px;
+    outline: none;
+  }
+
+  /*Experimental avatar*/
+  .avatar {
+    height: 200px;
+    position: relative;
+    width: 200px;
+  }
+  .avatar img {
+    border-radius: 9999px;
+    height: 100%;
+    position: relative;
+    width: 100%;
+    z-index: 2;
+  }
+
+  .grid__item {
+    outline: none;
+  }
+
+  @keyframes cycle-colors {
+    0% {
+      border-color: hsl(0, 100%, 50%);
+    }
+    25% {
+      border-color: hsl(90, 100%, 50%);
+    }
+    50% {
+      border-color: hsl(180, 100%, 50%);
+    }
+    75% {
+      border-color: hsl(270, 100%, 50%);
+    }
+    100% {
+      border-color: hsl(360, 100%, 50%);
+    }
+  }
+  @keyframes pulse {
+    to {
+      opacity: 0;
+      transform: scale(1);
+    }
+  }
+
+  .avatar::before,
+  .avatar::after {
+    animation: pulse 5s linear infinite;
+    border: #fff solid 5px;
+    border-radius: 99999px;
+    box-sizing: border-box;
+    content: " ";
+    height: 140%;
+    left: -20%;
+    opacity: 0.6;
+    position: absolute;
+    top: -20%;
+    transform: scale(0.714);
+    width: 140%;
+    z-index: 1;
+  }
+  .avatar::after {
+    animation-delay: 2s;
+  }
+
+  /* Fixed anchor click only button and centered*/
+  .anchorControl {
+    width: 107px;
+    text-align: center;
+    display: table;
+    margin: 0 auto;
+  }
 
   .glow-on-hover {
     width: 180px;
     height: 50px;
     border: none;
     outline: none;
-    color: #fff;
+    color: #ff3e00;
     background: #111;
     cursor: pointer;
     position: relative;
     z-index: 0;
     border-radius: 10px;
     font-weight: 600;
-    font-size: 20px;
+    font-size: 18px;
   }
 
   .glow-on-hover:before {
-    content: '';
-    background: linear-gradient(45deg, #ff0000, #ff7300, #fffb00, #48ff00, #00ffd5, #002bff, #7a00ff, #ff00c8, #ff0000);
+    content: "";
+    background: linear-gradient(
+      45deg,
+      #ff0000,
+      #ff7300,
+      #fffb00,
+      #48ff00,
+      #00ffd5,
+      #002bff,
+      #7a00ff,
+      #ff00c8,
+      #ff0000
+    );
     position: absolute;
     top: -2px;
-    left:-2px;
+    left: -2px;
     background-size: 400%;
     z-index: -1;
     filter: blur(5px);
@@ -151,42 +167,45 @@
     height: calc(100% + 4px);
     animation: glowing 20s linear infinite;
     opacity: 0;
-    transition: opacity .3s ease-in-out;
+    transition: opacity 0.3s ease-in-out;
     border-radius: 10px;
   }
 
   .glow-on-hover:active {
-    color: #000
+    color: #000;
   }
 
-    .glow-on-hover:active:after {
-        background: transparent;
+  .glow-on-hover:active:after {
+    background: transparent;
+  }
+
+  .glow-on-hover:hover:before {
+    opacity: 1;
+  }
+
+  .glow-on-hover:after {
+    z-index: -1;
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: #111;
+    left: 0;
+    top: 0;
+    border-radius: 10px;
+  }
+
+  @keyframes glowing {
+    0% {
+      background-position: 0 0;
     }
-
-    .glow-on-hover:hover:before {
-        opacity: 1;
+    50% {
+      background-position: 400% 0;
     }
-
-    .glow-on-hover:after {
-        z-index: -1;
-        content: '';
-        position: absolute;
-        width: 100%;
-        height: 100%;
-        background: #111;
-        left: 0;
-        top: 0;
-        border-radius: 10px;
+    100% {
+      background-position: 0 0;
     }
-
-    @keyframes glowing {
-        0% { background-position: 0 0; }
-        50% { background-position: 400% 0; }
-        100% { background-position: 0 0; }
-    }
-
-
-
+  }
 </style>
 
 <svelte:head>
