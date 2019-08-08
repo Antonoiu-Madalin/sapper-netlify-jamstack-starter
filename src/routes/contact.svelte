15d5add9f3850994ea51e3c1db8b1e81e8aa097f
Antonoiu Madalin <20832842+Antonoiu-Madalin@users.noreply.github.com>
2019-08-08 15:56:53 +0300

    Contact fixed.

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -1,13 +1,9 @@
-<svelte:head>
-	<title>Contact | Send email</title>
-</svelte:head>
-
-
 <script>
   import { createEventDispatcher } from "svelte";
   import Button from "../components/Button.svelte";
   import TextInput from "../components/TextInput.svelte";
   import { isEmpty, isValidEmail } from "../helpers/validation.js";
+  import { fade, fly } from "svelte/transition";
 
   export let id = null;
 
@@ -84,83 +80,111 @@
     user-select: none;
   }
 
-  a{
-      -webkit-user-select: none;
-  -khtml-user-select: none;
-  -moz-user-select: none;
-  -o-user-select: none;
-  user-select: none;
+  a {
+    -webkit-user-select: none;
+    -khtml-user-select: none;
+    -moz-user-select: none;
+    -o-user-select: none;
+    user-select: none;
+  }
+
+  .nospace {
+    margin-left: -4px;
   }
 </style>
 
+<svelte:head>
+  <title>Contact | Send email</title>
+</svelte:head>
 <h1 style="color: white; ">Contact me</h1>
-<hr>
+<hr style="max-width: 300px; margin-left: 0;" />
 
+<li>
+  <a
+    href="https://join.slack.com/t/hireadevio/shared_invite/enQtNzE5ODg0Mjg0OTQ5LTQ1MjljY2VhMGYzZDg1ZDdhMjg1ZThhM2M2OTYyNTI1NDg1MmQwOTdjMWM3YTk5YzNhYTBiOGVjNzE0MzRkOTE">
+    <label for="tab2">
+      Join my
+      <span style="color: #36C5F0">S</span>
+      <span class="nospace" style="color: #2EB67D">l</span>
+      <span class="nospace" style="color: #E01E5A">a</span>
+      <span class="nospace" style="color: #ECB22E">c</span>
+      <span class="nospace" style="color: #ECB22E">k</span>
+      channel
+    </label>
+  </a>
+</li>
 
+<li>
 
+  <a
+    href="https://twitter.com/antonoiumadalin?ref_src=twsrc%5Etfw"
+    class="twitter-follow-button"
+    data-show-count="false">
+    <label>
+      <i class="fab fa-twitter" style="color: #1da1f2;">&nbsp;</i>
+      Follow on
+      <span style="color: #1da1f2;">Twitter</span>
+      <span style="color: #20B2AA" />
+    </label>
+  </a>
+</li>
 
-    <li>
-      <a href="https://join.slack.com/t/hireadevio/shared_invite/enQtNzE5ODg0Mjg0OTQ5LTQ1MjljY2VhMGYzZDg1ZDdhMjg1ZThhM2M2OTYyNTI1NDg1MmQwOTdjMWM3YTk5YzNhYTBiOGVjNzE0MzRkOTE">
-        <label for="tab2"><span style="color: #228B22">Join</span> my Slack channel</label>
-      </a>
-    </li>
+<li>
+  <label
+    on:click={() => {
+      sendMessage = !sendMessage;
+    }}>
+    <i class="fas fa-envelope" style="color: white;">&nbsp;</i>
+    Send me a
+    <span style="color: #20B2AA">message</span>
+  </label>
 
-    <li>
-      <label><i class="fab fa-twitter" style="color: #1da1f2;"> &nbsp; </i> Follow on Tweeter <span style="color: #20B2AA"></span></label>
-    </li>
+</li>
 
-    <li>
-      <label on:click={() => {sendMessage = !sendMessage} }><i class="fas fa-envelope" style="color: white;"> &nbsp; </i> Send me a <span style="color: #20B2AA">message</span></label>
-  
-    </li>
+{#if sendMessage}
+  <div class="container" in:fly={{ y: 200, duration: 1500 }} out:fade>
+    <form
+      id="contact-form"
+      method="post"
+      action="https://briskforms.com/go/eacf0e72a032519cba8ae9ce8d390439">
 
+      <TextInput
+        label="Full Name"
+        name="fullname"
+        on:input={event => (fullname = event.target.value)}
+        value={fullname}
+        valid={fullnameValid}
+        validityMessage="Please enter your full name."
+        type="text"
+        id="fname"
+        placeholder="Your full name.." />
 
+      <TextInput
+        name="email"
+        id="email"
+        label="E-Mail"
+        type="email"
+        valid={emailValid}
+        validityMessage="Please enter a valid email address."
+        value={email}
+        on:input={event => (email = event.target.value)}
+        placeholder="Your email adress.." />
 
+      <TextInput
+        name="message"
+        id="description"
+        label="Description"
+        controlType="textarea"
+        valid={descriptionValid}
+        validityMessage="Please enter a valid message."
+        bind:value={description}
+        type="textarea"
+        placeholder="Write me something.."
+        rows="10" />
 
-{#if sendMessage} 
-
-<div class="container">
-  <form id='contact-form' method="post" action="https://briskforms.com/go/eacf0e72a032519cba8ae9ce8d390439">
-    
-    <TextInput
-      label="Full Name"
-      name="fullname" 
-      on:input={event => (fullname = event.target.value)}  
-      value={fullname} valid={fullnameValid}   
-      validityMessage="Please enter your full name." 
-      type="text" 
-      id="fname" 
-      placeholder="Your full name.."
-    />
-    
-    <TextInput
-      name="email"
-      id="email"
-      label="E-Mail"
-      type="email"
-      valid={emailValid}
-      validityMessage="Please enter a valid email address."
-      value={email}
-      on:input={event => (email = event.target.value)}
-      placeholder="Your email adress.."
-    />
-
-    <TextInput
-      name="message"
-      id="description"
-      label="Description"
-      controlType="textarea"
-      valid={descriptionValid}
-      validityMessage="Please enter a valid message."
-      bind:value={description} 
-      type="textarea"
-      placeholder="Write me something.."
-      rows="10"
-    />
-      
       <Button type="submit" value="Submit" disabled={!formIsValid}>Send</Button>
 
-  </form>
-  
-</div>
+    </form>
+
+  </div>
 {/if}
